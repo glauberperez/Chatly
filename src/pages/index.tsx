3dba7b896fbb02e4103b911b1e71d0aa2467aa9d
glauberperez <glauber2070@hotmail.com>
2023-03-26 18:25:03 -0300

    base / index is something now

---
+++ src/pages/index.tsx
@@ -0,0 +1,27 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '@/styles/index.module.css'
+
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Chatly</title>
+        <meta name="description" content="Chatly - meet new people!" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main className="bg-light-purple flex flex-col items-center justify-center h-screen">
+      <Image
+          src="/chatly_main.png"
+          alt="Chatly"
+          width={500}
+          height={500}
+          className="pointer-events-none"
+        />
+        <button className="font-sanfrancisco mt-4 px-4 py-2 rounded-lg text-3xl">Join</button>
+      </main>
+    </>
+  )
+}
---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+ 
+    // Or if using `src` directory:
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+    colors: {
+      'light-purple': '#b4a7d6',
+    },
+    fontFamily: {
+      sanfrancisco: ["San Francisco", "sans-serif"],
+
+    },
+  },
+  plugins: [],
+}
